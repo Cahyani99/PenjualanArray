a4234f7620bb03d437e22a97631f803101a9fc5f
Bela Cahyani <ciciymelaniy99@gmail.com>
2025-11-04 16:19:55 +0700

    Commit 1 - setup awal

---
+++ array.php
@@ -0,0 +1,11 @@
+
+<?php
+// Commit 1 – Setup awal
+$kode_barang = ["BRG001", "BRG002", "BRG003", "BRG004", "BRG005"];
+$nama_barang = ["Teh Pucuk", "Sukro", "Sprite", "Chitato", "Oreo"];
+$harga_barang = [5000, 1000, 4000, 8000, 6000];
+
+echo "<b>--POLGAN MART--</b><br>";
+echo "Daftar Pembelian<br><br>";
+
+?>
